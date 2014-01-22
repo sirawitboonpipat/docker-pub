4678b1d791f63855de4d1a75d1a401704e28c13a
James McKernan <james.mckernan@hvflabs.com>
2014-01-22 11:15:15 -0800

    make latest tagging better; base readme updates

---
+++ redis/README.md
@@ -0,0 +1,19 @@
+Redis On Docker
+==========
+Building redis for use with docker. We break things up into 3 logical kinds of
+containers:
+
+Base: used to install the version of redis we want. Versions are 2.6.16 and
+2.8.1. NOTE that each of these Dockerfile ADD the a tarball of the form
+redis-{version}.tar.gz so those must be in the appropriate directory. They can
+be dowloaded from: http://download.redis.io/releases/
+Standalone: uses base as a, well, base, and adds directives like EXPOSE and
+VOLUME and ENTRYPOINT. Call a "runtime container."
+Slave: adds more runtime directives as Standalone, but also add a
+start-slave.sh script. This script which uses the env vars created with running
+the container with -link.
+
+To build these images, a build script is included. Execute ./build.sh to build
+all image types and then docker images to inspect them.
+
+
--- redis/build.sh
+++ redis/build.sh
@@ -2,14 +2,19 @@
 #
 docker build -t jamesm/redis-base:2.6.16 base/2.6.16
 docker build -t jamesm/redis-base:2.8.1 base/2.8.1
+LATEST_VERS=2.8.1
 # this image_id hockyness should be elliminated when this bug comes down in
 # 0.8.0: https://github.com/dotcloud/docker/issues/2659
 #
-IMAGE_ID_2_6_16=$(docker images jamesm/redis-base | grep 2.6.16 | awk '{print $3}')
-IMAGE_ID_2_8_1=$(docker images jamesm/redis-base | grep 2.8.1 | awk '{print $3}')
-docker tag $IMAGE_ID_2_8_1 jamesm/redis-base:latest
+IMAGE_ID_LATEST_BASE=$(docker images jamesm/redis-base | grep $LATEST_VERS | awk '{print $3}')
+docker tag $IMAGE_ID_LATEST_BASE jamesm/redis-base:latest
+
 docker build -t jamesm/redis-standalone:2.8.1 standalone
 docker build -t jamesm/redis-slave:2.8.1 slave
-docker tag $IMAGE_ID_2_6_16 jamesm/redis-base:latest
 docker build -t jamesm/redis-standalone:2.6.16 standalone
 docker build -t jamesm/redis-slave:2.6.16 slave
+
+IMAGE_ID_LATEST_SDLONE=$(docker images jamesm/redis-standalone | grep $LATEST_VERS | awk '{print $3}')
+docker tag $IMAGE_ID_LATEST_SDLONE jamesm/redis-standalone:latest
+IMAGE_ID_LATEST_SLAVE=$(docker images jamesm/redis-slave | grep $LATEST_VERS | awk '{print $3}')
+docker tag $IMAGE_ID_LATEST_SLAVE jamesm/redis-slave:latest
