093ad1ef51b9e4c01d84be8a03ecd657c203d136
James McKernan <james.mckernan@hvflabs.com>
2014-01-31 10:33:41 -0800

    cleanup for publication

--- redis/slave/start-slave.sh
+++ redis/slave/start-slave.sh
@@ -4,7 +4,6 @@ if [ -z "$REDIS_MASTER_PORT_6379_TCP_ADDR" ]; then
     echo "REDIS_MASTER_PORT_6379_TCP_ADDR not defined. Did you run with -link?";
     exit 7;
 fi
-# exec replaces the shell process with redis-server, which allows 
-# redis-server to receive signals for clean shutdown
+# exec allows redis-server to receive signals for clean shutdown
 #
 exec /usr/local/bin/redis-server --slaveof $REDIS_MASTER_PORT_6379_TCP_ADDR $REDIS_MASTER_PORT_6379_TCP_PORT $*
