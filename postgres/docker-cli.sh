a2ecf40daf0bd073be966e67566edebda4c4de19
James McKernan <james.mckernan@hvflabs.com>
2014-04-21 11:20:49 -0700

    better cli and data dir setup steps

--- postgres/docker-cli.sh
+++ postgres/docker-cli.sh
@@ -1,15 +1,38 @@
 #!/bin/bash
 #
 
-NAME=pg-cli
-PG_CONTAINER=pg-test
-
-# try to run it, if you can then try too attach to it
-#
-docker run -i -t --link=$PG_CONTAINER:pg --name=$NAME \
-    jamesm/postgres-base /bin/bash
-
-if [ $? -ne 0 ]; then 
-    echo "Attempting to attach to $NAME, might need to hit enter a few times... "
-    docker attach $NAME
+if [ $# != 1 ]; then
+    echo "usage: docker-cli.sh {pg container to link to}";
+    exit 1
 fi
+
+PG_CONTAINER=$1
+NAME=$1-cli
+
+C_STATE=$(docker inspect -f '{{.State.Running}}' $NAME)
+if [ $? -ne 0 ]; then
+    echo "Seems lik container never created. Doing so now....";
+    # try to run it, if you can then try too attach to it
+    #
+    docker run -d -i -t --link=$PG_CONTAINER:pg --name=$NAME \
+        jamesm/postgres-base:latest /bin/bash
+    if [ $? -ne 0 ]; then
+        echo "Unable to create $NAME. Exiting... ";
+        exit 1;
+    else 
+        C_STATE=$(docker inspect -f '{{.State.Running}}' $NAME)
+    fi
+
+fi
+if [ $C_STATE == "false" ]; then
+    echo "$NAME stopped, starting... ";
+    docker start $NAME
+    if [ $? -ne 0 ]; then 
+        echo "Unable to start $NAME, exiting... ";
+        exit 1;
+    fi
+    C_STATE=$(docker inspect -f '{{.State.Running}}' $NAME)
+fi
+echo "$NAME running: $C_STATE"
+echo "Attaching... (may have to hit enter a few times to see prompt... "
+docker attach $NAME
